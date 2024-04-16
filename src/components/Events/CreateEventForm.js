c1563599da9a46dab8dd78d105aa1688d0455ccd
Мария <m.kondratyeva.m@gmail.com>
2024-04-16 22:46:19 +0700

    test

--- src/components/Events/CreateEventForm.js
+++ src/components/Events/CreateEventForm.js
@@ -27,31 +27,38 @@ function CreateEventForm() {
 
   return (
     <div className="form-container">
-      <h3>Запланировать игру</h3>
+      <h5 className="form-title">Запланировать игру</h5>
       <form onSubmit={handleSubmit}>
-        <label>
-          <input placeholder="Название вида спорта" type="text" value={sport} onChange={(e) => setSport(e.target.value)} required/>
-        </label>
-        <select id="city" name="city" value={city} onChange={(e) => setCity(e.target.value)} required>
-          <option value="">Город проведения</option>
-          <option value="mos">Москва</option>
-          <option value="spb">Санкт-Петербург</option>
-          <option value="ekb">Екатеринбург</option>
-          <option value="krsk">Красноярск</option>
-          <option value="nsk">Новосибирск</option>
-          <option value="tsk">Томск</option>
-        </select>
-        <label>
-          Дата проведения:
-          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required/>
-        </label>
-        <label>
-          Время проведения:
-          <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required/>
-        </label>
-        <label>
-        <textarea placeholder="Расскажи о себе: какими видами спорта интересуешься, какие навыки имеешь" id="about" name="about" value={skills} onChange={(e) => setSkills(e.target.value)} rows="4" required />
-        </label>
+        <div className="form-group">
+          <label>
+            Название вида спорта:
+            <input placeholder="Название вида спорта" type="text" value={sport} onChange={(e) => setSport(e.target.value)} required/>
+          </label>
+          <label>
+            Город проведения:
+            <select id="city" name="city" value={city} onChange={(e) => setCity(e.target.value)} required>
+              <option value="">Город проведения</option>
+              <option value="mos">Москва</option>
+              <option value="spb">Санкт-Петербург</option>
+              <option value="ekb">Екатеринбург</option>
+              <option value="krsk">Красноярск</option>
+              <option value="nsk">Новосибирск</option>
+              <option value="tsk">Томск</option>
+            </select>
+          </label>
+          <label>
+            Дата проведения:
+            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required/>
+          </label>
+          <label>
+            Время проведения:
+            <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required/>
+          </label>
+          <label>
+            Расскажи о себе:
+            <textarea placeholder="Расскажи о себе: какими видами спорта интересуешься, какие навыки имеешь" id="about" name="about" value={skills} onChange={(e) => setSkills(e.target.value)} rows="4" required />
+          </label>
+        </div>
         <div className="button-group">
           <button type="submit" className="create-button">Создать</button>
           <button type="button" className="back-button">Назад</button>
