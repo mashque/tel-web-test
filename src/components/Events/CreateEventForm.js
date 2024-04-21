c84fb6c0ab0ecd47602366572a0e77d25d3c0fe1
Мария <m.kondratyeva.m@gmail.com>
2024-04-21 20:07:15 +0700

    tedt

--- src/components/Events/CreateEventForm.js
+++ src/components/Events/CreateEventForm.js
@@ -34,14 +34,7 @@ function CreateEventForm({ tg }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formattedDate = eventData.date?.toISOString().split('T')[0];
-    const formattedTime = eventData.time?.toISOString().split('T')[1].slice(0, 5);
-    const formattedData = {
-      ...eventData,
-      date: formattedDate,
-      time: formattedTime
-    };
-    tg.sendData(JSON.stringify(formattedData));
+    tg.sendData(JSON.stringify(eventData));
   };
 
   return (
